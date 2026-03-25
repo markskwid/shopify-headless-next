f290966921ff9bb99b0ba74675ac4108b727f6d6
markskwid <bascal.markponce.f.1015@gmail.com>
2026-03-25 16:58:31 +0800

    Adjusted header

--- components/Header/NavUser.tsx
+++ components/Header/NavUser.tsx
@@ -12,6 +12,7 @@ import { useEffect, useState } from "react";
 import { searchResults } from "@/lib/shopify/api/search";
 import { PRODUCT_SEARCH_TYPE } from "@/types/product";
 import Link from "next/link";
+import Image from "next/image";
 
 export const NavUser = () => {
   const { cart } = useCart();
@@ -49,37 +50,51 @@ export const NavUser = () => {
 
   return (
     <div className="flex justify-end items-center gap-3 [&_i]:text-2xl">
-      <div className="relative">
-        <div>
+      <div className="input-container absolute left-1/2 right-0 -translate-x-1/2 opacity-0 translate-y-full -bottom-10 w-[90%] md:relative md:translate-0 md:left-0 md:bottom-0 md:translate-y-0 md:opacity-100">
+        <form method="POST" className="">
           <input
             onChange={handleInputChange}
             type="text"
             tabIndex={1}
-            className="border bg-gray-300/50 rounded-md py-1 px-2 shadow-2xl w-72 hidden md:block"
+            className="border bg-gray-300/50 rounded-md py-1 px-2 shadow-2xl w-full md:w-72"
             placeholder="Search item"
           />
 
-          <button className="md:absolute md:right-2 md:top-1 align-middle">
+          <button type="submit" className="absolute right-2 top-1 align-middle">
             <i>
               <AiOutlineSearch />
             </i>
           </button>
-        </div>
+        </form>
 
         {searchData && (
           <div className="absolute bg-white w-full border border-gray-400 mt-2 rounded-md px-2 left-0 right-0">
             {searchData.map((item: PRODUCT_SEARCH_TYPE) => (
               <Link
+                key={item.id}
                 href={item.handle}
                 className="flex justify-start items-center my-2"
               >
-                <img width={50} src={item.featuredImage?.url} className="mr-2" />
+                <div className="relative mr-2">
+                  <Image
+                    src={item.featuredImage?.url ?? ""}
+                    width="50"
+                    alt={item.title}
+                    height="50"
+                    loading={"lazy"}
+                  />
+                </div>
                 <p>{item.title}</p>
               </Link>
             ))}
           </div>
         )}
       </div>
+      <button className="md:hidden align-middle">
+        <i>
+          <AiOutlineSearch />
+        </i>
+      </button>
       <button>
         <i>
           <AiOutlineUser />
