bef19a52eaeb5d3611fc8ed3b96b1bc1ef466c59
markskwid <bascal.markponce.f.1015@gmail.com>
2026-05-22 16:51:44 +0800

    fix: pricing for single variant product

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -4,24 +4,32 @@ import { ImageAndSwatches } from "./ImageAndSwatches";
 
 export const ProductCard = ({ product }: { product: PRODUCT_LISTING_TYPE }) => {
   return (
-    <div
-      key={product.id}
-      className="group w-full z-10"
-    >
+    <div key={product.id} className="group w-full z-10">
       <ImageAndSwatches product={product} />
       <h3 className="text-center font-semibold mt-2 text-xl">
         {product.title}
       </h3>
 
       <span className="text-center w-full inline-block my-1 font-semibold text-lg">
-        {formatPrice(
-          product.priceRange.minVariantPrice.amount,
-          product.priceRange.minVariantPrice.currencyCode,
-        )}{" "}
-        &ndash;{" "}
-        {formatPrice(
-          product.priceRange.maxVariantPrice.amount,
-          product.priceRange.maxVariantPrice.currencyCode,
+        {product.variants.nodes.length > 1 ? (
+          <>
+            {formatPrice(
+              product.priceRange.minVariantPrice.amount,
+              product.priceRange.minVariantPrice.currencyCode,
+            )}{" "}
+            &ndash;{" "}
+            {formatPrice(
+              product.priceRange.maxVariantPrice.amount,
+              product.priceRange.maxVariantPrice.currencyCode,
+            )}
+          </>
+        ) : (
+          <>
+            {formatPrice(
+              product.priceRange.minVariantPrice.amount,
+              product.priceRange.minVariantPrice.currencyCode,
+            )}
+          </>
         )}
       </span>
     </div>
