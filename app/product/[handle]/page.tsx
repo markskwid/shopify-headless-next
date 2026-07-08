a55100a0e5de19e715f98607861acac5e2eac2a8
markskwid <bascal.markponce.f.1015@gmail.com>
2026-07-08 11:43:21 +0800

    fix: delete the sleep (test) on pdp

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -16,8 +16,6 @@ interface Props {
   searchParams: Promise<{ size?: string }>;
 }
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export async function generateMetadata({ params }: any): Promise<Metadata> {
   const { handle } = await params;
   const product = await getProductByHandle(handle);
@@ -60,7 +58,6 @@ export async function generateMetadata({ params }: any): Promise<Metadata> {
 }
 
 export default async function ProductPage({ params, searchParams }: Props) {
-    await sleep(10000);
   const { handle } = await params;
   const { size } = await searchParams;
 
